47e5e750477793715fc37f478da8955f608ef01b
soutammolick829 <soutammollick829@gmail.com>
2023-07-22 04:08:25 +0600

    load data from server

--- index.js
+++ index.js
@@ -30,12 +30,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db('voiceHunterDB').collection('prooducts');
+
+    app.get("/products", async(req,res)=>{
+        const result = await productsCollection.find().toArray();
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
